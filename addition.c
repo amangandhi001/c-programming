240b7e049e852299fede14bb6ed34526bf3b40b5
gandhiaman001 <57363370+gandhiaman001@users.noreply.github.com>
2019-11-16 10:59:15 +0530

    Update addition.c

--- addition.c
+++ addition.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
-int main(){
-for(int i=1;i<=4;i++)
-    {
-       for(int j=1;j<=5;j++){
-if(j>=i && j<=6-i)
-printf("1");
-else
-printf("0");
-}
-printf("\n");
-}
+int main()
+{       
+ int a,b;
+ printf("Enter two num:");
+ scanf("%d %d",&a,&b);
+ printf("%d+%d=%d\n",a,b,a+b);
+         
+return 0;   
 }
